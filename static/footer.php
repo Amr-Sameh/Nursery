d9314a75f441689ab5b089989b2d8ccf6433db8a
Amr-Sameh <amrsameh.develop@gmail.com>
2017-03-07 13:42:39 +0200

    project stucture

---
+++ static/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC - MeiR
+ * Date: 3/7/2017
+ * Time: 1:05 PM
+ */
+?>
+<script src="js/wow.min.js"></script>
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
---
+++ static/header.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: PC - MeiR
+ * Date: 3/7/2017
+ * Time: 12:59 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/animate.css" >
+    <link rel="stylesheet" href="css/font-awesome.min.css" >
+
+
+
+
+
+
+
