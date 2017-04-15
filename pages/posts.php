61d9ec1ba450aa8309322a2571ed179bc18921e3
Omer Mohamed Rashad <Omer Mohamed Rashad>
2017-04-15 11:20:31 +0200

    First Design of posts

---
+++ pages/posts.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<title>Posts</title>
+	<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/posts.css">
+	<!--[if it IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+
+<!--gt Wrapper Start-->  
+    <!--Main Content Wrap Start-->
+    <div class="gt_main_content_wrap">
+        <!--News Grid Wrap Start-->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="row">
+                            <div class="col-md-6 col-md-6">
+                                <div class="gt_blog_wrap mb">
+                                    <figure>
+                                        <img src="" alt="">
+                                        <figcaption class="gt_blog_figcaption">
+                                            <ul>
+                                                <li><a href="#"><i class="fa fa-search"></i></a></li>
+                                                <li><a href="#"><i class="fa fa-expand"></i></a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="gt_blog_des_wrap">
+                                        <ul class="gt_blog_meta">
+                                            <li><i class="fa fa-calendar"></i>15 Sep 2017</li>
+                                            <li><i class="fa fa-heart"></i>17</li>
+                                            <li><i class="fa fa-comments"></i>04</li>
+                                        </ul>
+                                        <h5><a href="#">How kids think about earth</a></h5>
+                                        <p>Lorem Ipsum proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumnibh id elit. </p>
+                                        <a href="#">Read More <i class="fa fa-arrow-circle-o-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-md-6">
+                                <div class="gt_blog_wrap mb">
+                                    <figure>
+                                        <img src="" alt="">
+                                        <figcaption class="gt_blog_figcaption">
+                                            <ul>
+                                                <li><a href="#"><i class="fa fa-search"></i></a></li>
+                                                <li><a href="#"><i class="fa fa-expand"></i></a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="gt_blog_des_wrap">
+                                        <ul class="gt_blog_meta">
+                                            <li><i class="fa fa-calendar"></i>15 Sep 2017</li>
+                                            <li><i class="fa fa-heart"></i>17</li>
+                                            <li><i class="fa fa-comments"></i>04</li>
+                                        </ul>
+                                        <h5><a href="#">How kids think about earth</a></h5>
+                                        <p>Lorem Ipsum proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumnibh id elit. </p>
+                                        <a href="#">Read More <i class="fa fa-arrow-circle-o-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-md-6">
+                                <div class="gt_blog_wrap mb">
+                                    <figure>
+                                        <img src="" alt="">
+                                        <figcaption class="gt_blog_figcaption">
+                                            <ul>
+                                                <li><a href="#"><i class="fa fa-search"></i></a></li>
+                                                <li><a href="#"><i class="fa fa-expand"></i></a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="gt_blog_des_wrap">
+                                        <ul class="gt_blog_meta">
+                                            <li><i class="fa fa-calendar"></i>15 Sep 2017</li>
+                                            <li><i class="fa fa-heart"></i>17</li>
+                                            <li><i class="fa fa-comments"></i>04</li>
+                                        </ul>
+                                        <h5><a href="#">How kids think about earth</a></h5>
+                                        <p>Lorem Ipsum proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumnibh id elit. </p>
+                                        <a href="#">Read More <i class="fa fa-arrow-circle-o-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-md-6">
+                                <div class="gt_blog_wrap mb">
+                                    <figure>
+                                        <img src="" alt="">
+                                        <figcaption class="gt_blog_figcaption">
+                                            <ul>
+                                                <li><a href="#"><i class="fa fa-search"></i></a></li>
+                                                <li><a href="#"><i class="fa fa-expand"></i></a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="gt_blog_des_wrap">
+                                        <ul class="gt_blog_meta">
+                                            <li><i class="fa fa-calendar" aria-hidden="true"></i>15 Sep 2017</li>
+                                            <li><i class="fa fa-heart"></i>17</li>
+                                            <li><i class="fa fa-comments"></i>04</li>
+                                        </ul>
+                                        <h5><a href="#">How kids think about earth</a></h5>
+                                        <p>Lorem Ipsum proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumnibh id elit. </p>
+                                        <a href="#">Read More <i class="fa fa-arrow-circle-o-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- This Pagination must be auto with the average of posts number -->
+                        <!--Pagination Wrap Start-->
+                        <div class="gt_pagination_outer_wrap">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-angle-double-left"></i></a></li>
+                                <li class="active"><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
+                            </ul>
+                        </div>
+                    <!--Pagination Wrap End-->
+                    </div>
+
+                    
+                    <!--Aside Bar Wrap Start--><div class="col-md-3">
+                        <aside class="gt_aside_outer_wrap">  
+                            <!--Category Wrap Start-->
+                            <div class="gt_aside_category aside_margin_bottom">
+                                <ul>
+                                    <li><a href="#">Home Work</a></li>
+                                    <li><a href="#">Ask</a></li>
+                                    <li><a href="#">Events</a></li>
+                                    <li><a href="#">Posts</a></li>
+                                    <li><a href="#">Teachers</a></li>
+                                    <li><a href="#">TimeTable</a></li>
+                                </ul>
+                            </div>
+                            <!--Category Wrap Start-->
+                            
+                            <!--Recent News Wrap Start-->
+                            <div class="gt_aside_post_wrap aside_margin_bottom">
+                                <h6>Last Posts</h6>
+                                <ul>
+                                    <li>
+                                        <figure>
+                                            <img src="" alt="">
+                                        </figure>
+                                        <div class="gt_aside_post_des">
+                                            <span>Post 1</span>
+                                            <h6><a href="#">All Is New</a></h6>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <figure>
+                                            <img src="" alt="">
+                                        </figure>
+                                        <div class="gt_aside_post_des">
+                                            <span>Post 2</span>
+                                            <h6><a href="#">All Is New</a></h6>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <figure>
+                                            <img src="" alt="">
+                                        </figure>
+                                        <div class="gt_aside_post_des">
+                                            <span>Post 3</span>
+                                            <h6><a href="#">All Is New</a></h6>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <figure>
+                                            <img src="" alt="">
+                                        </figure>
+                                        <div class="gt_aside_post_des">
+                                            <span>Post 4</span>
+                                            <h6><a href="#">All Is New</a></h6>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <figure>
+                                            <img src="" alt="">
+                                        </figure>
+                                        <div class="gt_aside_post_des">
+                                            <span>Post 5</span>
+                                            <h6><a href="#">All Is New</a></h6>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!--Recent News Wrap Start-->
+                            <!--Tags Wrap Start-->
+                        </aside>
+                    </div>
+                    <!--Aside Bar Wrap End-->
+                </div>
+            </div>
+        </section>
+        <!--News Grid Wrap End-->
+    </div>
+    <!--Main Content Wrap End-->
+    
+    <!--Back to Top Wrap Start-->
+    <!--Back to Top Wrap Start-->
+
+<!--gt Wrapper End-->
+
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
